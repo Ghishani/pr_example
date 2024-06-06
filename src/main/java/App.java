24b8ead1baee615af6c34ddbe689322942b6e6bc
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-06-06 12:24:08 +0100

    add to line 5 on main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
+        System.out.println("Added this to line 6 on main branch");;
     }
 }
